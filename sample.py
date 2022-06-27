f0932c7c25cfd42e032257597b4f084024bc4164
mi2valley <monocoin189@gmail.com>
2022-06-28 00:52:58 +0900

    fix env

--- sample.py
+++ sample.py
@@ -1,13 +1,8 @@
 import slackweb
 import os
-import argparse
-
 
 text = "test"
-#parser = argparse.ArgumentParser(description='slack env')
-#parser.add_argument('env', type=str)
 slack_id = os.getenv("SLACK_WEBHOOK_URL")
-#args = parser.parse_args()
 print(type(slack_id))
 """
 def main() -> None:
