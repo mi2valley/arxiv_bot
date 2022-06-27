63000ae00ac9e7032e72d897ab49bcffcf2d8865
mi2valley <monocoin189@gmail.com>
2022-06-28 03:31:36 +0900

    arxiv test

--- sample.py
+++ sample.py
@@ -6,20 +6,16 @@ text = "test"
 slack_id = os.getenv("SLACK_WEBHOOK_URL")
 author = "Saikat Guha"
 
-def au_search(author: str):
-  search = arxiv.Search(query ='au:"'+author+'"',max_results = 1,sort_by = arxiv.SortCriterion.SubmittedDate)
-  return search
-"""
-def main() -> None:
-    # slack
-    if slack_id is not None:
-        slack = slackweb.Slack(url=slack_id)
-        slack.notify(text="test")
-
-if __name__ == "__main__":
-    main()
-"""
-search = au_search(author)
-attachments = search.results()[0].summary
 slack = slackweb.Slack(url=str(slack_id))
-slack.notify(text=attachments)
+
+search = arxiv.Search(
+  query = "quantum",
+  max_results = 10,
+  sort_by = arxiv.SortCriterion.SubmittedDate
+)
+
+for result in search.results():
+  print(result.title)
+  slack.notify(text=result.title)
+
+
