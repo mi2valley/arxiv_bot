2449fa311105cfab04d04a9f2a47b38c750b6fb8
mi2valley <monocoin189@gmail.com>
2022-06-28 03:43:40 +0900

    arxiv test

--- sample.py
+++ sample.py
@@ -4,18 +4,31 @@ import arxiv
 
 text = "test"
 slack_id = os.getenv("SLACK_WEBHOOK_URL")
-author = "Saikat Guha"
+#author = "Saikat Guha"
 
 slack = slackweb.Slack(url=str(slack_id))
 
 search = arxiv.Search(
   query = "quantum",
-  max_results = 10,
+  max_results = 5,
   sort_by = arxiv.SortCriterion.SubmittedDate
 )
 
+
 for result in search.results():
-  print(result.title)
-  slack.notify(text=result.title)
+  attachments = [
+        {
+        "mrkdwn_in": ["text"],
+            "color": "#36a64f",
+            "author_name": result.authors[0],
+            "title": result.title,
+            "text": result.summary,
+            "thumb_url": "http://placekitten.com/g/200/200",
+            "footer": "footer",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789
+        }
+    ]
+  slack.notify(text=result.title, attachments=attachments)
 
 
