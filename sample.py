fc817bd86004d60467ad91bde75fc0d903461a92
mi2valley <monocoin189@gmail.com>
2022-06-28 00:28:17 +0900

    actions

--- sample.py
+++ sample.py
@@ -4,9 +4,9 @@ import argparse
 
 
 text = "test"
-parser = argparse.ArgumentParser(description='slack env')
-parser.add_argument('env', type=str)
-#slack_id = os.getenv("SLACK_INCOMING_WEBHOOK_URL")
+#parser = argparse.ArgumentParser(description='slack env')
+#parser.add_argument('env', type=str)
+slack_id = os.getenv("SLACK_INCOMING_WEBHOOK_URL")
 args = parser.parse_args()
 #print(type(slack_id))
 """
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     main()
 """
 
-slack = slackweb.Slack(url=args.env)
+slack = slackweb.Slack(url=str(slack_id))
 slack.notify(text=text)
